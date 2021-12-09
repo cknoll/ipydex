14622b67eb186173c3f1d17e4442efe607755785
Carsten Knoll <carsten.knoll@posteo.de>
2021-12-09 21:45:11 +0100

    refactor setup.py and package structure

--- setup.py
+++ setup.py
@@ -1,26 +1,54 @@
+
+import sys
+import os
+import setuptools
 from setuptools import setup, find_packages
-from ipydex.release import __version__
+
+
+packagename = "ipydex"
+
+# consider the path of `setup.py` as root directory:
+PROJECTROOT = os.path.dirname(sys.argv[0]) or "."
+release_path = os.path.join(PROJECTROOT, "src", packagename, "release.py")
+with open(release_path, encoding="utf8") as release_file:
+    __version__ = release_file.read().split('__version__ = "', 1)[1].split('"', 1)[0]
+
 
 with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read()
 
-setup(name='ipydex',
-      version=__version__,
-      description='IPython based debugging and exploring tool',
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Software Development',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Utilities'
-      ],
-      keywords='ipython embedded excepthook debugger ',
-      url='http://github.com/cknoll/ipydex',
-      author='cknoll',
-      author_email='carsten.knoll@tu-dresden.de',
-      license='GPLv3+',
-      install_requires=requirements,
-      setup_requires=requirements,
-      packages=find_packages(),
-      zip_safe=False)
+setup(
+    name=packagename,
+    version=__version__,
+    author="Carsten Knoll",
+    author_email="firstname.lastname@posteo.de",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url='http://github.com/cknoll/ipydex',
+    license="GPLv3+",
+    description='IPython based debugging and exploring tool',
+    long_description="""
+# IPython based debugging and exploring tool
+
+This package provides the following features:
+
+- `IPS()` – An embedded IPython shell with some extra features like conditional execution: `IPS(x>0)`.
+- `activate_ips_on_exception()` – Open an IPython shell in the frame where an exception is raised, but with the ability to move to other frames.
+- `dirsearch` – easily search for substrings in the attribute list of an object or in the keys of a dict.
+- `displaytools` – an extesion for jupyter notebooks to display intermediate results (e.g. of assignments). Useful for didactic purposes.
+""",
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords='ipython embedded excepthook debugger ',
+    zip_safe=False,
+)
+
+
+
+
